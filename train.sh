479376247f22832cb41411402172c9dfb0f83b15
liht1996 <liht1996@live.com>
2018-12-09 18:19:24 -0800

    90pts-3

--- train.sh
+++ train.sh
@@ -4,4 +4,4 @@
 #PBS -o ./output_log.txt
 
 cd ~/Modified
-python main.py -mode train -saved_model models/model.h5 -student_id 20328915 -epochs 10 -batch_size 32 -embedding_dim 200 -hidden_size 500 -drop 0.2
+python main.py -mode train -saved_model models/model.h5 -student_id 20328915 -epochs 15 -batch_size 32 -embedding_dim 200 -hidden_size 500 -drop 0.2
